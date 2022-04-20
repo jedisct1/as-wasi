358cc48953060a83b52413dff82cf6e5dd8e131c
Frank Denis <github@pureftpd.org>
2022-04-20 21:35:59 +0200

    Include as-wasi/assembly
    Adds to #121

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,4 @@
 export {
   WASIError, Descriptor,
   Console, Random, Date, Process, EnvironEntry, Environ, CommandLine, FileSystem, FileStat, Time
-} from "./as-wasi";
+} from "./as-wasi/assembly";
